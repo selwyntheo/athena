ca34c5ea5b5754f5ff564f7ea2219edbfa04636a
selwyntheo <selwyn.theo@gmail.com>
2016-07-10 10:06:02 -0400

    First Version

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def quiz():
     
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 9011))
-	app.run(host='0.0.0.0', port=9011, debug=True)
+	port = int(os.environ.get("PORT", 8000))
+	app.run(host='0.0.0.0', port=8000, debug=True)
